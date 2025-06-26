9378d4bd28a045fa217d62582dd29a7a2031e9c4
siva2j22 <siva.2j22@gmail.com>
2025-06-27 05:29:58 +0530

    on coditions loop

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -6,8 +6,9 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "EEROR: PLEASE RUN THIS script with root access"
+    exit 52 # it will stop here
 else
     echo  " you are root user"
-fi
+fi     #it was used to close the if statement 
 
 yum install mysql -y
