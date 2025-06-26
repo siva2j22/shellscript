89522c0202cdd11599171cc08f2be42cfc127967
siva2j22 <siva.2j22@gmail.com>
2025-06-26 11:49:24 +0530

    sample echo command

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your username"
+
+read -s USERNAME
+
+echo "please enter your password"
+
+read -s PASSWORD
+
+echo "username is" $USERNAME ,password : $PASSWORD 
+#just printing this message
+#actually it will hide the data will executing by using -s
