acc82ee6fcc7b002d0fdceacf0a64f3c521bec8b
siva2j22 <siva.2j22@gmail.com>
2025-06-26 11:38:05 +0530

    sample echo command

---
+++ 02-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=jakkam
+PERSON2=siva
+
+echo  "$PERSON1 : Hello $PERSON2, GoodMorning"
+echo "$PERSON2: Hi $PERSON1, very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1, How are you"
