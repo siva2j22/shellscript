0659a214fe5016d2ca679c9105f0c67c36e5cfaa
siva2j22 <siva.2j22@gmail.com>
2025-06-27 11:22:32 +0530

    adding log files

--- 11-logs.sh
+++ 11-logs.sh
@@ -18,19 +18,19 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "ERROR:: $2 is $R failed $N"
+        echo -e "ERROR:: $2 is $R failed $N"
         exit 25
     else
-        echo " installed software $2 $G success $N"
+        echo -e " installed software $2 $G success $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo " $R EEROR: PLEASE RUN THIS script with root access $N"
+    echo -e " $R EEROR: PLEASE RUN THIS script with root access $N"
     exit 52 # it will stop here
 else
-    echo  " $G you are root user $N"
+    echo  -e " $G you are root user $N"
 fi     #it was used to close the if statement 
 
 
