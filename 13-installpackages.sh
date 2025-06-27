eee88f70f85acc4756d1f57bcfe8adab64b85e8a
siva2j22 <siva.2j22@gmail.com>
2025-06-27 12:59:39 +0530

    adding files

---
+++ 13-installpackages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R EEROR: PLEASE RUN THIS script with root access $N"
+    exit 52 # it will stop here
+else
+    echo  -e " $G you are root user $N"
+fi     #it was used to close the if statement 
+
+echo "i want all the arguments passed :$@'
