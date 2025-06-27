093066b47cd7af9e9b4dbd9298f60c45749ed907
siva2j22 <siva.2j22@gmail.com>
2025-06-27 10:32:27 +0530

    adding log files

---
+++ 11-logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name :$0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "ERROR:: $2 is failed"
+        exit 25
+    else
+        echo " installed software $2 success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "EEROR: PLEASE RUN THIS script with root access"
+    exit 52 # it will stop here
+else
+    echo  " you are root user"
+fi     #it was used to close the if statement 
+
+
+yum install mysql -y 
+
+VALIDATE $? "installed mysql" 
+
+yum install git -y
+
+VALIDATE $? "INSTALLING GIT"
